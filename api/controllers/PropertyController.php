eb1fb5fb2674b792dd5dc627a6bd782c876d2d9d
Bing Fang <bfang99@yahoo.com>
2015-10-14 17:00:17 -0400

    alon amount breaking options margin breaking to confirming and nonconfirming

--- api/controllers/PropertyController.php
+++ api/controllers/PropertyController.php
@@ -3,7 +3,8 @@
 class  PropertyController extends BaseController {
 
 	private $inputs;
-
+    private $property;
+	
 	function __construct($inputForm) {
 		$this->inputs = $inputForm;
 	}
@@ -12,23 +13,33 @@ class  PropertyController extends BaseController {
     	
     	$property = new LoanProperty($this->inputs);
     	$property->printProperty();
+    	$myoptions = $property->loanAmountOptions;
+
+    	foreach ($myoptions as $opt)
     	
-    	$myFeeCalculator = new FeeCalculator($property);
-    	$totalFee = $myFeeCalculator->getTotalFees();
-    	//echo "fees : " . $totalFee;
+     	$myFeeCalculator = new FeeCalculator($property);
+     	$totalFee = $myFeeCalculator->getTotalFees();
+     	//echo "fees : " . $totalFee;
     	
      	$purchasers=["BBT", "BOKF", "WELLSFARGO"];
      	foreach ($purchasers as $purchaser) {
-     		echo "========== purchaser $purchaser <br>" ;
-     		$purchaserCalculatorName  = $purchaser."RateCalculator" ;
-     		$myRateCalculator = new $purchaserCalculatorName ;
-     		$myRateCalculator->setProperty($property);
-     		$myRateCalculator->setTotalFee($totalFee);
-     		$myRateCalculator->calculteRate();
 
-     		echo "<hr>";
+     		foreach ($myoptions as $opt) {
+     			$property_clone = clone $property; 
+     		    echo "========== purchaser $purchaser, loan option = $opt[1] + $opt[2] <br>" ;
+     		    echo "========== Primary loan<br>";
+     		    $property_clone->loanAmount   = $opt[1];
+     		    $property_clone->calculateDerives();
+     		    $purchaserCalculatorName  = $purchaser."RateCalculator" ;
+     		    $myRateCalculator = new $purchaserCalculatorName ;
+     		    $myRateCalculator->setProperty($property_clone);
+     		    $myRateCalculator->setTotalFee($totalFee);
+     		    $myRateCalculator->calculteRate();
+     		    echo "========== Secondary loan<br>";
+     		    $myRateCalculator->calculteSecondaryRate( $opt[2] );
+     		    echo "<hr>";
+     		}
      	}
-       
     }
 
 }
