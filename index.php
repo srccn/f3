1e1a5585cc8198bf72be68f1eff33aa583f7cf07
Bing Fang <bfang99@yahoo.com>
2015-10-15 20:16:20 -0400

    add error echo

--- index.php
+++ index.php
@@ -2,4 +2,12 @@
 $f3 = require('../fatfree-master/lib/base.php');
 $f3->config('config/config.ini');
 $f3->config('config/route.ini');
+$f3->set('ONERROR',
+		function($f3) {
+			// custom error handler code goes here
+			// use this if you want to display errors in a
+			// format consistent with your site's theme
+			echo $f3->get('ERROR.text');
+		}
+		);
 $f3->run();
