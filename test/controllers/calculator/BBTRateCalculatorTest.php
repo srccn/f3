2069d6dceffe5475f3a787fffab8439af17d0162
Bing Fang <bfang99@yahoo.com>
2015-11-03 23:57:25 -0500

    more test on RateCalculator

--- test/controllers/calculator/BBTRateCalculatorTest.php
+++ test/controllers/calculator/BBTRateCalculatorTest.php
@@ -65,10 +65,33 @@ class BBTRateCalculatorTest extends PHPUnit_Framework_TestCase {
 		//var_dump($testAdjusts);
 		$this->assertEquals(-0.75, $testAdjusts['LtvCcAdj']); //740 with 0.80
 		$this->assertEquals(0, $testAdjusts['LtvCcPmiAdj']); //740 with 0.80
-		$this->assertEquals(0, $testAdjusts['LtvOtherAdj']); //740 with 0.80
-		
+		$this->assertEquals(0, $testAdjusts['LtvOtherAdj']); //740 with 0.80// 		$testCalculator = new BBTRateCalculator;
+
 	}
-	
+
+	/**
+	 * @test
+	 */
+	public function testGetAdjustsVary () {	
+		$testInputForm = new InputForm();
+		$testInputs = $testInputForm->getSampleForm();
+		
+		$testInputs['loanAmount']=295000;
+		$testInputs['creditScore']=710;
+		$testInputs['type']=LoanerConst::CONDO;
+		$testLoanProperty = new LoanProperty($testInputs);
+		
+		$testCalculator = new BBTRateCalculator;
+		$testCalculator->setProperty($testLoanProperty);
+		$testCalculator->setTotalFee(2500);
+		
+		$testCalculator->calculateAllAdjusts();
+		$testAdjusts = $testCalculator->getAdjusts();
+		$this->assertEquals(-1.0, $testAdjusts['LtvCcAdj']); //700 with 0.98
+		$this->assertEquals(-2.5, $testAdjusts['LtvCcPmiAdj']); //700 with 0.98
+		$this->assertEquals(-0.75, $testAdjusts['LtvOtherAdj']); //condo
+		$this->assertEquals(-4.25, $testCalculator->getTotalAdjusts());		
+	}
 	
 }
 
